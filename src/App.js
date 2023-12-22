bb4b9b5f067160146430fb9541b83537f668d71e
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2023-12-22 01:29:05 +0100

    bouton accueil et meilleure recherche

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const API_URL = 'https://www.omdbapi.com/?apikey=1b68ee50';
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [filmNotFound, setFilmNotFound] = useState(false);
 
     useEffect(() => {
         searchMovies("");
@@ -18,22 +19,39 @@ const App = () => {
             const response = await fetch(`${API_URL}&s=${title}`);
             const data = await response.json();
 
-            setMovies(data.Search);
+            if (data.Response === 'True') {
+                setMovies(data.Search);
+                setFilmNotFound(false);
+            } else {
+                setMovies([]);
+                setFilmNotFound(true);
+            }
+
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     };
+    
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            searchMovies(searchTerm);
+        }
+    };
 
+    const handleTitleClick = () => {
+        searchMovies("");
+        setSearchTerm("");
+    };
 
     return (<div className="app">
-        <h1>Moviepédia</h1>
+        <button className="button-title" onClick={handleTitleClick}><h1>Moviepedia</h1></button>
 
         <div className="search">
-
             <input
                 placeholder="Rechercher un film"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyDown={handleKeyDown}
             />
             <img
                 src={SearchIcon}
@@ -41,25 +59,21 @@ const App = () => {
                 onClick={() => searchMovies(searchTerm)}
             />
         </div>
-
         {
             movies?.length > 0
                 ? (
                     <div className="container">
-                        {movies.map((movie) =>(
+                        {movies.map((movie) => (
                             <MovieCard movie={movie} />
                         ))}
                     </div>
-                ) :(
+                ) : (
                     <div className="empty">
                         <h2>L'encyclopédie cinématographique que le monde attendait</h2>
                     </div>
                 )
         }
-
-
     </div>
-
     );
 }
 
