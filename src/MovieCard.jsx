4d4c8a937e4f17141e20d9d442c90b8a29ce298b
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2023-12-28 17:01:02 +0100

    fini

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const MovieCard = ({ movie }) => {
+    const [plot, setPlot] = useState('');
+
+    useEffect(() => {
+        const fetchPlot = async () => {
+            try {
+                const response = await fetch(`${'https://www.omdbapi.com/?apikey=d25ba319'}&i=${movie.imdbID}`);
+                const data = await response.json();
+                setPlot(data.Plot || 'No plot available.');
+            } catch (error) {
+                console.error('Error fetching plot:', error);
+            }
+        };
+
+        fetchPlot();
+    }, [movie.imdbID]);
+
     return (
-            <div className="movie">
-                <div>
-                    <p>{movie.Year}</p>
-                </div>
-                <div>
-                    <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
-                </div>
-                <div>
-                    <span>{movie.Type}</span>
-                    <h3>{movie.Title}</h3>
-                </div>
+        <div className="movie">
+            <div>
+                <h3>{movie.Year}</h3>
+                <br></br>
+                <p>{plot}</p>
             </div>
-            );
-}
+            <div>
+                <img src={movie.Poster} alt={movie.Title} />
+            </div>
+            <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
+            </div>
+        </div>
+    );
+};
 
 export default MovieCard;
